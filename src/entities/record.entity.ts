9b339be220f47d74eeead3166ad238abf50c172e
Insiro <leeon@insiro.me>
2023-08-27 13:22:58 +0900

    update: record entity

--- src/entities/record.entity.ts
+++ src/entities/record.entity.ts
@@ -1,7 +1,17 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class Record {
     @PrimaryColumn()
     student_id!:string;
+    @Column()
+    clear_time:number
+    @Column()
+    name:string
+    @Column()
+    department:string
+    @Column()
+    phone:string
+    @Column()
+    nickname:string
 }
